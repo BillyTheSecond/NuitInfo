42f00dda74c29545d96fdc5e817e05ef698b76c3
maxlem24 <lemaitre.maxime2004@gmail.com>
2023-12-08 06:19:48 +0100

    feat(html): Creation des pages articles de 1 a 10 feat(html): ajout de contenu(fictif) aux pages articles feat(html): ajout de contenu réel + images aux pages articles

--- nuitInfo/core/urls.py
+++ nuitInfo/core/urls.py
@@ -5,4 +5,14 @@ app_name = 'core'
 urlpatterns = [
     path('', views.index, name='index'),
     path('velosaure/', views.velosaure, name='velosaure'),
+    path('article1/', views.article1, name='article1'),
+    path('article2/', views.article2, name='article2'),
+    path('article3/', views.article3, name='article3'),
+    path('article4/', views.article4, name='article4'),
+    path('article5/', views.article5, name='article5'),
+    path('article6/', views.article6, name='article6'),
+    path('article7/', views.article7, name='article7'),
+    path('article8/', views.article8, name='article8'),
+    path('article9/', views.article9, name='article9'),
+    path('article10/', views.article10, name='article10'),
 ]
