c9d356fcf85f1d863e3b8e942d40d3d08ced3359
DYSTOpyy <bcd.saltel@gmail.com>
2023-12-08 04:00:06 +0100

    feat: base responsive & début index.html

---
+++ nuitInfo/static/js/base.js
@@ -0,0 +1,6 @@
+// init
+function changerEtatMenu() {
+    document.getElementById("menu-icon").classList.toggle("ferme");
+    document.getElementById("header-right").classList.toggle("ferme");
+  
+  }
