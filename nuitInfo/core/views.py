455d740f04b11b45ad79a4944b4b6bbe40f9f31f
mael_adv <advissemael@gmail.com>
2023-12-08 04:50:04 +0100

    feat(html): Creation des pages articles de 1 a 10

--- nuitInfo/core/views.py
+++ nuitInfo/core/views.py
@@ -3,3 +3,33 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html')
+
+def article1(request):
+    return render(request, 'core/article1.html')
+
+def article2(request):
+    return render(request, 'core/article2.html')
+
+def article3(request):  
+    return render(request, 'core/article3.html')
+
+def article4(request):  
+    return render(request, 'core/article4.html')
+
+def article5(request):  
+    return render(request, 'core/article5.html')
+
+def article6(request):  
+    return render(request, 'core/article6.html')
+
+def article7(request):  
+    return render(request, 'core/article7.html')
+
+def article8(request):  
+    return render(request, 'core/article8.html')
+
+def article9(request):  
+    return render(request, 'core/article9.html')
+
+def article10(request):  
+    return render(request, 'core/article10.html')
