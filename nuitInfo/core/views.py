c219c3e39b6aaa5752165fec0c07fe57c9c134d8
Prismey <prismey@murena.io>
2023-12-08 06:49:38 +0100

    feat (css) : modification du css de contact

--- nuitInfo/core/views.py
+++ nuitInfo/core/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'core/index.html')
 
+def contact(request) :
+    return render(request, 'core/contact.html')
+
 def velosaure(request):
     return render(request, 'core/velosaure.html')
 
